9acc9049c5fb1739eb142bf10c045686a70f33b4
s-sirakiLab <s-siraki@ist.osaka-u.ac.jp>
2018-08-09 11:29:40 +0900

    it can do plus and minus.

--- Main.py
+++ Main.py
@@ -7,9 +7,24 @@ Created on Thu Aug  9 10:59:45 2018
 
 import sys
 
-a = int(input('Inuput 1st Number.'))
-b = int(input('Inuput 2nd Number.'))
+def plus(a,b):
+    return a+b
 
-x = a + b
+def minus(a,b):
+    return a-b
+
+a = int(input('Inuput 1st Number.\n'))
+b = int(input('Inuput 2nd Number.\n'))
+
+op = int(input('Choice operation. Plus:1, Minus:2\n'))
+
+if op == 1:
+    x = plus(a,b)
+elif op ==2:
+    x= minus(a,b)
+else:
+    print('Error:Invalid　Operator.')
+    sys.exit(1)
 
 print('The answer is ' + str(x) + '.')
+
