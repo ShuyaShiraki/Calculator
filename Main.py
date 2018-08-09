c0650ecd9beab336101f7a7a73bcde2e106225bf
s-sirakiLab <s-siraki@ist.osaka-u.ac.jp>
2018-08-09 11:10:15 +0900

    Create Main

---
+++ Main.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  9 10:59:45 2018
+
+@author: s-siraki
+"""
+
+import sys
+
+a = int(input('Inuput 1st Number.'))
+b = int(input('Inuput 2nd Number.'))
+
+x = a + b
+
+print('The answer is ' + str(x) + '.')
